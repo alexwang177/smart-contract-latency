1020efae0c65a74210d8149bff2a41a7d9c4b557
Alex Wang <jianuo.wang@duke.edu>
2022-03-30 19:25:04 -0400

    name registry contract

---
+++ contracts/nameRegistry.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract nameRegistry {
+    mapping(string => string) map;
+    mapping(string => address) authMap;
+
+    // Register new name, value pair
+    function register(string memory name, string memory value)
+        public
+        returns (bool)
+    {
+        bytes memory mapValue = bytes(map[name]);
+        if (mapValue.length == 0) {
+            // key not found
+            map[name] = value;
+            authMap[name] = msg.sender;
+            return true;
+        } else {
+            if (authMap[name] == msg.sender) {
+                map[name] = value;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Reads the last stored value
+    function read(string memory name) public view returns (string memory) {
+        return map[name];
+    }
+}
